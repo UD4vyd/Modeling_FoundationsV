af68c21970df7bd74586c091677272451f199d03
UD4vyd <davidmartinezrincon9@gmail.com>
2024-09-20 13:35:25 -0500

    asjdaksjd

---
+++ Pruebas de commit/masprubeasjajaj.py
@@ -0,0 +1,5 @@
+print("Voy a probar si puedo haver un pull desde la tablet")
+
+"""
+pero que esta pasndo que no me hace los commits bien si si hay mas cosas que agragra y el hp dice que esta carpeta está vacia
+"""
