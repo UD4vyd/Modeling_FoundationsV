4a14ae6ac312d71bdbf8024380ea97d4dd712ebf
UD4vyd <davidmartinezrincon9@gmail.com>
2024-09-20 18:40:25 +0000

    ultima prueba de commit

--- Pruebas de commit/masprubeasjajaj.py
+++ Pruebas de commit/masprubeasjajaj.py
@@ -3,3 +3,7 @@ print("Voy a probar si puedo haver un pull desde la tablet")
 """
 pero que esta pasndo que no me hace los commits bien si si hay mas cosas que agragra y el hp dice que esta carpeta está vacia
 """
+
+"""
+pues si se puede y estoy muy feliz que si se pueda y ya puedo ser re pro haciendo commits y eso dididios
+"""
